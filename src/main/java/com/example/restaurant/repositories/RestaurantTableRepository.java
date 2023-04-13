fae022b6e76304e293876572ed939fd1f6cd53fd
mciuculan <mciuculan@gmail.com>
2023-04-14 01:12:22 +0300

    pana aici merge

--- src/main/java/com/example/restaurant/repositories/RestaurantTableRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restaurant.repositories;
-
-import com.example.restaurant.entities.RestaurantTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Integer> {
-}
